db222d40c3d397107262280588c465a721288c96
JuanCruzBarberis <juanbarberis.97@gmail.com>
2024-08-02 10:08:17 -0300

    obejto de selectOption

---
+++ src/utils/cameraRover.js
@@ -0,0 +1,54 @@
+const cameraRover = [
+    {
+        key: 1,
+        camera: 'FHAZ',
+        name: 'Front Hazard Avoidance Camera',
+    },
+    {
+        key: 2,
+        camera: 'RHAZ',
+        name: 'Rear Hazard Avoidance Camera',
+    },
+    {
+        key: 3,
+        camera: 'MAST',
+        name: 'Mast Camera',
+    },
+    {
+        key: 4,
+        camera: 'CHEMCAM',
+        name: 'Chemistry and Camera Complex',
+    },
+    {
+        key: 5,
+        camera: 'MAHLI',
+        name: 'Mars Hand Lens Imager',
+    },
+    {
+        key: 6,
+        camera: 'MARDI',
+        name: 'Mars Descent Imager',
+    },
+    {
+        key: 7,
+        camera: 'NAVCAM',
+        name: 'Navigation Camera',
+    },
+    {
+        key: 8,
+        camera: 'PANCAM',
+        name: 'Panoramic Camera',
+    },
+    {
+        key: 9,
+        camera: 'MINITES',
+        name: 'Miniature Thermal Emission Spectrometer ',
+    },
+    {
+        key: 10,
+        camera: null,
+        name: 'All Camera',
+    },
+]
+
+export default cameraRover;
