933615d280f641f4b9a3c1f70a5cc8569a3d73e2
JuanCruzBarberis <juanbarberis.97@gmail.com>
2024-08-05 00:46:25 -0300

    se agrego el reducer

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar, StyleSheet, Text, View } from 'react-native';
 import StackNavigator from './src/navigation/stackNav/stackNavigator';
+import { Provider } from 'react-redux';
+import { store } from './src/state/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist'
 
+
+let persistor = persistStore(store)
 
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar backgroundColor={'white'} barStyle={'dark-content'} />
-      <StackNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <StatusBar backgroundColor={'white'} barStyle={'dark-content'} />
+          <StackNavigator />
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 }
 
