db53aa7abb3e71f59865b1d87f0ba249eb8b209a
JuanCruzBarberis <juanbarberis.97@gmail.com>
2024-08-05 08:14:02 -0300

    fix

--- src/components/customButton/CustomButtom.jsx
+++ src/components/customButton/CustomButtom.jsx
@@ -1,5 +1,6 @@
 import { View, Text, Button, StyleSheet } from 'react-native'
 import React from 'react'
+
 const CustomButton = ({ width, callback, color, text }) => {
     return (
         <View style={{ width }}>
--- src/components/customSlider/CustomSlider.jsx
+++ src/components/customSlider/CustomSlider.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View,  StyleSheet } from 'react-native';
 import Slider from '@react-native-community/slider';
 
 const CustomSlider = ({
